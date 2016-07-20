83b81fb2f183893b74003b114bf3ab83f988f21e
znddzxx112 <znddzxx112@163.com>
2016-07-20 16:09:24 +0800

    debug_backtrace,error_reporting

---
+++ tests/errorfun/errorClassTest.php
@@ -0,0 +1,28 @@
+<?php 
+require __dir__.'/../../vendor/autoload.php';
+
+require __dir__.'/../../src/errorfun/errorClass.php';
+
+/**
+* error
+*/
+class ErrorClassTest extends PHPUnit_Framework_TestCase
+{
+	
+	public function test_debug_back_trace()
+	{
+		$obj = new \Znddzxx112\Errorfun\ErrorClass();
+		$backtrace_arr = $obj->debug_back_trace();
+		$this->assertTrue(is_array($backtrace_arr));
+	}
+
+	public function test_get_error_get_last()
+	{
+		$obj = new \Znddzxx112\Errorfun\ErrorClass();
+		$error_get_last_arr = $obj->get_error_get_last();
+		if(!is_null($error_get_last_arr)){
+			$this->assertTrue(is_array($error_get_last_arr));
+		}
+	}
+
+}
