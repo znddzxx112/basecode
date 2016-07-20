4377bb547b0b6474d3909fadeff92689eb4a4215
znddzxx112 <znddzxx112@163.com>
2016-07-20 14:19:25 +0800

    dir,opendir,readdir,rewinddir,closedir,chdir,scandir

---
+++ src/directoryfun/directoryClass.php
@@ -0,0 +1,68 @@
+<?php 
+namespace Znddzxx112\Directoryfun;
+
+/**
+* directory
+* opendir
+* readdir
+* closedir
+* $dir = dir()
+* $dir->read()
+* $dir->close()
+*/
+class DirectoryClass
+{
+
+	private $_dir_handle=null;
+
+	public function __construct($directory='')
+	{
+		if($directory != ''){
+			if(($dir_handle = opendir($directory)) !== false){
+				$this->_dir_handle = $dir_handle;
+			}
+		}
+	}	
+
+	public function get_pwd()
+	{
+		return getcwd();
+	}
+
+	public function change_dir($directory)
+	{
+		return chdir($directory);
+	}
+
+	public function scan_dir($directory)
+	{
+		return scandir($directory);
+	}
+
+	public function read_dir()
+	{
+		if($this->_dir_handle==null){
+			return false;
+		}
+		$dir_files = array();
+		while(($file = readdir($this->_dir_handle))!=false){
+			$dir_files[] = $file;
+		}
+		rewinddir($this->_dir_handle);
+		return $dir_files;
+	}
+
+	public function dir_obj($directory='')
+	{
+		return dir($directory);
+	}
+
+	public function __destruct()
+	{
+		if($this->_dir_handle!=null){
+			closedir($this->_dir_handle);
+		}
+	}
+
+}
+
---
+++ tests/directoryfun/directoryClassTest.php
@@ -0,0 +1,52 @@
+<?php 
+require __dir__.'/../../vendor/autoload.php';
+
+require __dir__.'/../../src/directoryfun/directoryClass.php';
+
+/**
+* directoryClass
+*/
+class DirectoryClass extends PHPUnit_Framework_TestCase
+{
+	public function test_change_dir_pwd()
+	{
+		$obj = new \Znddzxx112\Directoryfun\DirectoryClass();
+		$cwd = $obj->get_pwd();
+		$pos = strrpos($cwd, "\\");
+		$dirname = ltrim(substr($cwd, $pos),"\\");
+		$this->assertEquals("directoryfun",$dirname);
+		$obj->change_dir("../datefun/");
+		$cwd = $obj->get_pwd();
+		$pos = strrpos($cwd, "\\");
+		$dirname = ltrim(substr($cwd, $pos),"\\");
+		$this->assertEquals("datefun",$dirname);
+	}
+
+	public function test_dir_fun()
+	{
+		$obj = new \Znddzxx112\Directoryfun\DirectoryClass(__dir__);
+		$dir_files = $obj->read_dir();
+		$this->assertTrue(in_array("directoryClassTest.php", $dir_files));
+	}
+
+	public function test_dir_obj()
+	{
+		$obj = new \Znddzxx112\Directoryfun\DirectoryClass();
+		$odir = $obj->dir_obj(__dir__);
+		$dir_files = array();
+		while(( $file = $odir->read() ) != false){
+			$dir_files[] = $file;
+		}
+		$odir->rewind();
+		$odir->close();
+		$this->assertTrue(in_array("directoryClassTest.php", $dir_files));
+	}
+	
+	public function test_scan_dir()
+	{
+		$obj = new \Znddzxx112\Directoryfun\DirectoryClass();
+		$dir_files = $obj->scan_dir(__dir__);
+		$this->assertTrue(in_array("directoryClassTest.php", $dir_files));
+	}
+
+}
